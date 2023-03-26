a58abcc89718da8ce85b2b025041bcd27e4960f1
RukeshRukku <guditirukesh668@gmail.com>
2023-03-26 02:02:32 +0000

    Wrapper component (ProtectedRoute)  is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import {Switch, Route, Redirect} from 'react-router-dom'
 import Login from './components/Login'
 import NotFound from './components/NotFound'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
 
 // use the below bookshelvesList for rendering read status of book items in Bookshelves Route
 
@@ -31,6 +33,7 @@ const bookshelvesList = [
 const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
     <Route exact path="/not-found" component={NotFound} />
     <Redirect to="/not-found" />
   </Switch>
